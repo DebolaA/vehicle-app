0aec728aaaf8267b7e68bc2bc9f6872aeabf5c68
Debola Adedapo <niikee80@hotmail.com>
2025-08-28 13:12:17 +0100

    refactor of orderby directive

--- src/app/directive/orderBy.directive.ts
+++ src/app/directive/orderBy.directive.ts
@@ -9,24 +9,27 @@ import {
   Renderer2,
 } from '@angular/core';
 import { ISelectOption } from '../model/VehicleModel';
+import { BehaviorSubject } from 'rxjs';
+import { SortDropdownComponent } from '../components/sort-dropdown/sort-dropdown';
 
 @Directive({
   selector: '[orderBy]',
 })
 export class OrderByDirective {
-  constructor(private el: ElementRef, private renderer: Renderer2) {}
+  constructor(
+    private el: ElementRef,
+    private renderer: Renderer2,
+    private dropdownComponent: SortDropdownComponent
+  ) {}
 
   @Input()
   optionList: ISelectOption[] = [];
-
-  @Output()
-  emittedValue = new EventEmitter<ISelectOption>();
 
   ngOnInit() {}
 
   @HostListener('click') mouseover() {
     let option: number = this.el.nativeElement.selectedIndex;
     if (option === 0) return;
-    this.emittedValue.emit(this.optionList[option]);
+    this.dropdownComponent.selectedOption$.next(option);
   }
 }
